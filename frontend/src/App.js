1b96facfe09b3d349dcfb8d79dca65053814ead4
gillesretiere <gilles.retiere@gmail.com>
2023-05-30 14:08:39 +0000

    added router 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn with Langdeck 
         </a>
       </header>
     </div>
