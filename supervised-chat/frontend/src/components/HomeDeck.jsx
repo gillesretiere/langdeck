842b6e65f8d6037ccf04d25b156434211ceec069
gillesretiere <gilles.retiere@gmail.com>
2024-02-21 20:24:10 +0000

    CSS

--- supervised-chat/frontend/src/components/HomeDeck.jsx
+++ supervised-chat/frontend/src/components/HomeDeck.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import classes from "./HomeDeck.module.css";
 
 const HomeDeck = ({startingDeck}) => {
   return (
-    <div>
+    <div className={classes.container}>
         {startingDeck && startingDeck.map(
             (el) => {
-                return (<div> {el.lang_name_fr}</div>)
+                return (
+                <div className={classes.card__container__blue}> 
+                  <img src={el.lang_flag_icon} className={classes.card_img}></img>
+                  <h3 class="card-name">
+                    {el.lang_name_fr}
+                  </h3>
+                </div>)
             }
         )
         }        
