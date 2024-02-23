56704414af634f80c735f14c4fcd70e5c8cf0b52
gillesretiere <gilles.retiere@gmail.com>
2024-02-23 08:48:21 +0000

    HomeDeck Entire Layout : conversation dock (top), left, right, deck base (bottom dock)

--- supervised-chat/frontend/src/components/HomeDeck.jsx
+++ supervised-chat/frontend/src/components/HomeDeck.jsx
@@ -5,6 +5,7 @@ import HomeDeckList from './HomeDeckList';
 const HomeDeck = ({startingDeck}) => {
 
   return (
+    <>
     <div className={classes.container}>
       <div className={classes.left_container}>
         <div className={classes.card_container}>
@@ -12,13 +13,25 @@ const HomeDeck = ({startingDeck}) => {
           <HomeDeckList deck={startingDeck}/>
           <div className={classes.card_footer}></div>
         </div>
-
       </div>
       <div className={classes.right_container}>
-        <div>En teete</div>
-        <HomeDeckList deck={startingDeck}/>
+        <div>Conversation</div>
       </div> 
     </div>
+    <div className={classes.deck_base_container}>
+    {startingDeck && startingDeck.map(
+                    (el) => {
+                        return (
+                          <div className={classes.card_container}>
+                          <div className={classes.card_header}>Choisir une langue</div>
+                          <HomeDeckList deck={startingDeck}/>
+                          <div className={classes.card_footer}></div>
+                        </div>
+                            )
+                    }
+                )} 
+    </div>
+    </>
   )
 }
 
--- supervised-chat/frontend/src/components/UI/Footer.jsx
+++ supervised-chat/frontend/src/components/UI/Footer.jsx
@@ -5,8 +5,8 @@ import classes from "./Layout.module.css";
 const Footer = () => {
   return (
     <>
-      <div className="h-full shadow-inner p-5 overflow-hidden">
-        <nav className="flex justify-start relative items-center h-16">
+      <div className="h-24 shadow-inner p-5 overflow-hidden">
+        <nav className="flex justify-start relative items-center">
           <div className={`${classes.logo}`}>
             <img className={`${classes.logo}`} src='https://res.cloudinary.com/dhc7ovnwk/image/upload/v1703342819/hammer-marteau/othy1coezxdmnxvkhkmw.png'></img>
             <span> | &copy;Hammer & Marteau 2024</span> 
